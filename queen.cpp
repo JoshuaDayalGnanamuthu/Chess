52db417c66d425e18359ce89ef309560831edf07
Joshua <joshuadayal72@gmail.com>
2026-03-28 18:41:49 -0400

    Created a Cpp File with the Queen class implementation. Has methods to return valid move set.

---
+++ queen.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+typedef std::vector<std::vector<std::string>> board;
+struct Position {
+    int posY = -1;
+    int posX = -1;
+};
+
+class Queen {
+    public:
+        std::string piece;
+        std::string type = "Q";
+        std::string number;
+        Position position; 
+        bool isWhite;
+
+        Queen(std::string number, bool isWhite = true) 
+        {
+            this->isWhite = isWhite;
+            this->number = number;
+            this->piece = ((this->isWhite) ? "w" : "b") + this->type + this->number;
+            this->position = piece_positions.at(this->piece);
+        }
+
+        std::vector<Position> validMoves(const board &chess_board){
+            std::vector<Position> moveset = {};
+            bool topLeft = true;
+            bool topRight = true;
+            bool bottomLeft = true;
+            bool bottomRight = true;
+
+            for ( int i = 1; i < 8; i ++)
+            {
+                if (topRight) 
+                {
+                    int row = this->position.posY - i;
+                    int col = this->position.posX + i;
+                    if (row < 0 || col > 7) {
+                        topRight = false;
+                    }
+                    else
+                    {
+                        std::string piece = chess_board[row][col];
+                        if (piece == " ")
+                        {
+                            Position new_position = {row, col};
+                            moveset.push_back(new_position);
+                        }
+                        else 
+                        {
+                            if ((this->isWhite && piece.substr(0, 1) == "b") || (!this->isWhite && piece.substr(0, 1) == "w"))
+                            {
+                                Position new_position = {row, col};
+                                moveset.push_back(new_position);
+                            }
+                            topRight = false;
+                        }
+                    }
+                }
+
+                if (topLeft) 
+                {
+                    int row = this->position.posY - i;
+                    int col = this->position.posX - i;
+                    if (row < 0 || col < 0) {
+                        topLeft = false;
+                    }
+                    else
+                    {
+                        std::string piece = chess_board[row][col];
+                        if (piece == " ")
+                        {
+                            Position new_position = {row, col};
+                            moveset.push_back(new_position);
+                        }
+                        else 
+                        {
+                            if ((this->isWhite && piece.substr(0, 1) == "b") || (!this->isWhite && piece.substr(0, 1) == "w"))
+                            {
+                                Position new_position = {row, col};
+                                moveset.push_back(new_position);
+                            }
+                            topLeft = false;
+                        }
+                    }
+                }
+
+                if (bottomRight) 
+                {
+                    int row = this->position.posY + i;
+                    int col = this->position.posX + i;
+                    if (row > 7 || col > 7) {
+                        bottomRight = false;
+                    }
+                    else
+                    {
+                        std::string piece = chess_board[row][col];
+                        if (piece == " ")
+                        {
+                            Position new_position = {row, col};
+                            moveset.push_back(new_position);
+                        }
+                        else 
+                        {
+                            if ((this->isWhite && piece.substr(0, 1) == "b") || (!this->isWhite && piece.substr(0, 1) == "w"))
+                            {
+                                Position new_position = {row, col};
+                                moveset.push_back(new_position);
+                            }
+                            bottomRight = false;
+                        }
+                    }
+                }
+
+                if (bottomLeft) 
+                {
+                    int row = this->position.posY + i;
+                    int col = this->position.posX - i;
+                    if (row > 7 || col < 0) {
+                        bottomLeft = false;
+                    }
+                    else
+                    {
+                        std::string piece = chess_board[row][col];
+                        if (piece == " ")
+                        {
+                            Position new_position = {row, col};
+                            moveset.push_back(new_position);
+                        }
+                        else 
+                        {
+                            if ((this->isWhite && piece.substr(0, 1) == "b") || (!this->isWhite && piece.substr(0, 1) == "w"))
+                            {
+                                Position new_position = {row, col};
+                                moveset.push_back(new_position);
+                            }
+                            bottomLeft = false;
+                        }
+                    }
+                }
+            }
+
+            for ( int row = this->position.posY + 1; row < 8; row++ ) 
+            {
+                int col = this->position.posX;
+                if ( chess_board[row][col] == " "){
+                    Position new_position = {row, col};
+                    moveset.push_back(new_position);
+                }
+                else 
+                {
+                    std::string piece = chess_board[row][col];
+                    if (this->isWhite && piece.substr(0, 1) == "b")
+                    {
+                        Position new_position = {row, col};
+                        moveset.push_back(new_position);
+                    }
+                    else if (!this->isWhite && piece.substr(0, 1) == "w")
+                    {
+                        Position new_position = {row, col};
+                        moveset.push_back(new_position);
+                    }
+                    break;
+                }
+            }
+
+            for ( int row = this->position.posY - 1; row >= 0; row-- ) 
+            {
+                int col = this->position.posX;
+                if ( chess_board[row][col] == " "){
+                    Position new_position = {row, col};
+                    moveset.push_back(new_position);
+                }
+                else 
+                {
+                    std::string piece = chess_board[row][col];
+                    if (this->isWhite && piece.substr(0, 1) == "b")
+                    {
+                        Position new_position = {row, col};
+                        moveset.push_back(new_position);
+                    }
+                    else if (!this->isWhite && piece.substr(0, 1) == "w")
+                    {
+                        Position new_position = {row, col};
+                        moveset.push_back(new_position);
+                    }
+                    break;
+                }
+            }
+
+            for ( int col = this->position.posX + 1; col < 8; col++ ) 
+            {
+                int row = this->position.posY;
+                if ( chess_board[row][col] == " "){
+                    Position new_position = {row, col};
+                    moveset.push_back(new_position);
+                }
+                else 
+                {
+                    std::string piece = chess_board[row][col];
+                    if (this->isWhite && piece.substr(0, 1) == "b")
+                    {
+                        Position new_position = {row, col};
+                        moveset.push_back(new_position);
+                    }
+                    else if (!this->isWhite && piece.substr(0, 1) == "w")
+                    {
+                        Position new_position = {row, col};
+                        moveset.push_back(new_position);
+                    }
+                    break;
+                }
+            }
+
+            for ( int col = this->position.posX - 1; col >= 0; col-- ) 
+            {
+                int row = this->position.posY;
+                if ( chess_board[row][col] == " "){
+                    Position new_position = {row, col};
+                    moveset.push_back(new_position);
+                }
+                else 
+                {
+                    std::string piece = chess_board[row][col];
+                    if (this->isWhite && piece.substr(0, 1) == "b")
+                    {
+                        Position new_position = {row, col};
+                        moveset.push_back(new_position);
+                    }
+                    else if (!this->isWhite && piece.substr(0, 1) == "w")
+                    {
+                        Position new_position = {row, col};
+                        moveset.push_back(new_position);
+                    }
+                    break;
+                }
+            }
+  
+            return moveset;
+        }
+
+
+    private:
+        static const std::map<std::string, Position> piece_positions;
+};
+
+const std::map<std::string, Position> Queen::piece_positions = 
+        {
+            {"bR2", {0, 0}}, {"bN2", {0, 1}}, {"bB2", {0, 2}}, {"bQ",  {0, 3}},
+            {"bK",  {0, 4}}, {"bB1", {0, 5}}, {"bN1", {0, 6}}, {"bR1", {0, 7}},
+            {"bP8", {1, 0}}, {"bP7", {1, 1}}, {"bP6", {1, 2}}, {"bP5", {1, 3}},
+            {"bP4", {1, 4}}, {"bP3", {1, 5}}, {"bP2", {1, 6}}, {"bP1", {1, 7}},
+            {"wP1", {6, 0}}, {"wP2", {6, 1}}, {"wP3", {6, 2}}, {"wP4", {6, 3}},
+            {"wP5", {6, 4}}, {"wP6", {6, 5}}, {"wP7", {6, 6}}, {"wP8", {6, 7}},
+            {"wR1", {7, 0}}, {"wN1", {7, 1}}, {"wB1", {7, 2}}, {"wQ",  {7, 3}},
+            {"wK",  {7, 4}}, {"wB2", {7, 5}}, {"wN2", {7, 6}}, {"wR2", {7, 7}}
+        };
+        
