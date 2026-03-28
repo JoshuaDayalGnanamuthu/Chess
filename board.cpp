6eb31a51b5fe76927e94ee3e072f07f750bdc02d
Joshua <joshuadayal72@gmail.com>
2026-03-28 11:43:28 -0400

    Implemented A Board Class public: board object, print board functions private: column alias

--- board.cpp
+++ board.cpp
@@ -3,67 +3,76 @@
 #include <vector>
 #include <map>
 
-typedef std::vector<std::vector<std::string>> Board;
-std::map<std::string, int> column_alias = {
-    {"a", 0},
-    {"b", 1},
-    {"c", 2},
-    {"d", 3},
-    {"e", 4},
-    {"f", 5},
-    {"g", 6},
-    {"h", 7}
+
+class Board {
+    typedef std::vector<std::vector<std::string>> board;
+    public:
+        board chess_board = {
+            {"♖","♘","♗","♕","♔","♗","♘","♖"},
+            {"♙","♙","♙","♙","♙","♙","♙","♙"},
+            {" "," "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "," "},
+            {" "," "," "," "," "," "," "," "},
+            {"♟","♟","♟","♟","♟","♟","♟","♟"},
+            {"♜","♞","♝","♛","♚","♝","♞","♜"}        
+        };
+
+        size_t num_rows = 8;
+        size_t num_cols = 8;
+
+        void print_board_white(void) {
+            std::cout << "\n    a   b   c   d   e   f   g   h\n";
+            std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+
+            for (int row = 0; row < 8; row++)
+            {
+                std::cout << (8 - row) << " |";
+                for (int col = 0; col < 8; col++)
+                {
+                    std::cout << " " << this->chess_board[row][col] << " " << "|";
+                }
+                std::cout << " " << (8 - row) << "\n";
+                std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+            }
+            std::cout << "    a   b   c   d   e   f   g   h\n\n";
+        }
+
+        void print_board_black(void) {
+            std::cout << "\n    a   b   c   d   e   f   g   h\n";
+            std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+
+            for (int row = 7; row >= 0; row--)
+            {
+                std::cout << (8 - row) << " |";
+                for (int col = 0; col < 8; col++)
+                {
+                    std::cout << " " << this->chess_board[row][col] << " " << "|";
+                }
+                std::cout << " " << (8 - row) << "\n";
+                std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+            }
+            std::cout << "    a   b   c   d   e   f   g   h\n\n";
+        }
+
+    private:
+        std::map<std::string, int> column_alias = {
+            {"a", 0},
+            {"b", 1},
+            {"c", 2},
+            {"d", 3},
+            {"e", 4},
+            {"f", 5},
+            {"g", 6},
+            {"h", 7}
+        };
 };
-
-Board board = {
-        {"♖","♘","♗","♕","♔","♗","♘","♖"},
-        {"♙","♙","♙","♙","♙","♙","♙","♙"},
-        {" "," "," "," "," "," "," "," "},
-        {" "," "," "," "," "," "," "," "},
-        {" "," "," "," "," "," "," "," "},
-        {" "," "," "," "," "," "," "," "},
-        {"♟","♟","♟","♟","♟","♟","♟","♟"},
-        {"♜","♞","♝","♛","♚","♝","♞","♜"}        
-    };
-
-void print_board_white(const Board &board) {
-    std::cout << "\n    a   b   c   d   e   f   g   h\n";
-    std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
-
-    for (int row = 0; row < 8; row++)
-    {
-        std::cout << (8 - row) << " |";
-        for (int col = 0; col < 8; col++)
-        {
-            std::cout << " " << board[row][col] << " " << "|";
-        }
-        std::cout << " " << (8 - row) << "\n";
-        std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
-    }
-
-    std::cout << "    a   b   c   d   e   f   g   h\n\n";
-}
-void print_board_black(const Board &board) {
-    std::cout << "\n    a   b   c   d   e   f   g   h\n";
-    std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
-
-    for (int row = 7; row >= 0; row--)
-    {
-        std::cout << (8 - row) << " |";
-        for (int col = 0; col < 8; col++)
-        {
-            std::cout << " " << board[row][col] << " " << "|";
-        }
-        std::cout << " " << (8 - row) << "\n";
-        std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
-    }
-
-    std::cout << "    a   b   c   d   e   f   g   h\n\n";
-}
 
 int main()
 {
-    print_board_white(board);
-    print_board_black(board);
+    Board board = Board();
+    board.print_board_black();
+    board.print_board_white();
+    std::cout << "Hello World \n" << std::endl;
     return 0;
 }
