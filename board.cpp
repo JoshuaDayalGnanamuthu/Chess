73448156aca63db832416e43f73d112894c90cc2
Joshua <joshuadayal72@gmail.com>
2026-03-28 14:47:23 -0400

    Updated Board Display logic to eliminate bugs. Use a map to store sprites, to enable board display to be dynamic

--- board.cpp
+++ board.cpp
@@ -3,94 +3,85 @@
 #include <vector>
 #include <map>
 
-#define RESET       "\033[0m"
-#define BG_LIGHT    "\033[48;2;240;217;181m"
-#define BG_DARK     "\033[48;2;181;136;101m"
-#define FG_WHITE    "\033[97m"
-#define FG_BLACK    "\033[30m"
 
 class Board {
     typedef std::vector<std::vector<std::string>> board;
     public:
+        std::map<std::string, int> column_alias = {
+            {"a", 0},
+            {"b", 1},
+            {"c", 2},
+            {"d", 3},
+            {"e", 4},
+            {"f", 5},
+            {"g", 6},
+            {"h", 7}
+        };
+        
         board chess_board = {
-            {"black_rook","black_knight","black_bishop","black_queen","black_king","black_bishop","black_knight","black_rook"},
-            {"black_pawn","black_pawn","black_pawn","black_pawn","black_pawn","black_pawn","black_pawn","black_pawn"},
+            {"bR2","bN2","bB2","bQ","bK","bB1","bN1","bR1"},
+            {"bP8","bP7","bP6","bP5","bP4","bP3","bP2","bP1"},
             {" "," "," "," "," "," "," "," "},
             {" "," "," "," "," "," "," "," "},
             {" "," "," "," "," "," "," "," "},
             {" "," "," "," "," "," "," "," "},
-            {"white_pawn","white_pawn","white_pawn","white_pawn","white_pawn","white_pawn","white_pawn","white_pawn"},
-            {"white_rook","white_knight","white_bishop","white_queen","white_king","white_bishop","white_knight","white_rook"}
+            {"wP1","wP2","wP3", "wP4","wP5","wP6","wP7","wP8"},
+            {"wR1","wN1","wB1","wQ","wK","wB2","wN2","wR2"}        
         };
 
-        void print_board_white(void) {
-            std::cout << "\n     a     b     c     d     e     f     g     h\n\n";
+        size_t num_rows = 8;
+        size_t num_cols = 8;
+
+        void printBoardWhite(void) {
+            std::cout << "\n    a   b   c   d   e   f   g   h\n";
+            std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+
             for (int row = 0; row < 8; row++)
             {
-
-                printPaddingRow(row);
-                std::cout << " " << (8 - row) << " ";
-                printPieceRow(row);
-                std::cout << "  " << (8 - row) << "\n";
-                printPaddingRow(row);
+                std::cout << (8 - row) << " |";
+                for (int col = 0; col < 8; col++)
+                {
+                    std::string piece = (this->chess_board[row][col]).substr(0,2);
+                    std::string sprite = sprites[piece];
+                    std::cout << " " << sprite << " " << "|";
+                }
+                std::cout << " " << (8 - row) << "\n";
+                std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
             }
-            std::cout << "\n     a     b     c     d     e     f     g     h\n\n";
+            std::cout << "    a   b   c   d   e   f   g   h\n\n";
         }
 
-        void print_board_black(void) {
-            std::cout << "\n     h     g     f     e     d     c     b     a\n\n";
+        void printBoardBlack(void) {
+            std::cout << "\n    h   g   f   e   d   c   b   a\n";
+            std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
+
             for (int row = 7; row >= 0; row--)
             {
-                printPaddingRow(row, true);
-                std::cout << " " << (8 - row) << " ";
-                printPieceRow(row, true);
-                std::cout << "  " << (8 - row) << "\n";
-                printPaddingRow(row, true);
+                std::cout << (8 - row) << " |";
+                for (int col = 7; col >= 0; col--)
+                {
+                    std::string piece = (this->chess_board[row][col]).substr(0, 2);
+                    std::string sprite = sprites[piece];
+                    std::cout << " " << sprite << " " << "|";
+                }
+                std::cout << " " << (8 - row) << "\n";
+                std::cout << "  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n";
             }
-            std::cout << "\n     h     g     f     e     d     c     b     a\n\n";
+            std::cout << "    h   g   f   e   d   c   b   a\n\n";
         }
 
     private:
         std::map<std::string, std::string> sprites = {
-            {"white_pawn",   "♙"}, {"white_rook",   "♖"}, {"white_knight", "♘"},
-            {"white_bishop", "♗"}, {"white_queen",  "♕"}, {"white_king",   "♔"},
-            {"black_pawn",   "♟"}, {"black_rook",   "♜"}, {"black_knight", "♞"},
-            {"black_bishop", "♝"}, {"black_queen",  "♛"}, {"black_king",   "♚"},
+            {"wP", "♟"}, {"wR", "♜"}, {"wN", "♞"}, {"wB", "♝"}, {"wQ", "♛"}, {"wK", "♚"}, 
+            {"bP", "♙"}, {"bR", "♖"}, {"bN", "♘"}, {"bB", "♗"}, {"bQ", "♕"}, {"bK", "♔"},
             {" ", " "}
         };
-
-        bool isWhitePiece(const std::string& piece) {
-            return piece.substr(0, 5) == "white";
-        }
-
-        std::string getBg(int row, int col) {
-            return ((row + col) % 2 == 0) ? BG_LIGHT : BG_DARK;
-        }
-
-        void printPaddingRow(int row, bool reversed = false) {
-            std::cout << "   ";
-            for (int i = 0; i < 8; i++) {
-                int col = reversed ? (7 - i) : i;
-                std::cout << getBg(row, col) << "      " << RESET;
-            }
-            std::cout << "\n";
-        }
-
-        void printPieceRow(int row, bool reversed = false) {
-            for (int i = 0; i < 8; i++) {
-                int col = reversed ? (7 - i) : i;
-                std::string bg = getBg(row, col);
-                std::string fg = isWhitePiece(chess_board[row][col]) ? FG_WHITE : FG_BLACK;
-                std::string sprite = sprites.count(chess_board[row][col]) ? sprites[chess_board[row][col]] : " ";
-                std::cout << bg << fg << "  " << sprite << "   " << RESET;
-            }
-        }
 };
 
 int main()
 {
     Board board = Board();
-    board.print_board_white();
-    board.print_board_black();
+    board.printBoardWhite();
+    board.printBoardBlack();
     return 0;
 }
