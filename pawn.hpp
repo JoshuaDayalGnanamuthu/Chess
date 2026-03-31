c32a73f0028523b1d9e16466a42e2d5dc635bf25
Joshua <joshuadayal72@gmail.com>
2026-03-30 23:10:10 -0400

    Split Pawn Class into a hpp & cpp file

---
+++ pawn.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include "piece.hpp"
+
+
+class Pawn: public Piece {
+    public:
+        bool hasMoved = false;
+        
+        Pawn(std::string number, Position position, bool isWhite) 
+        : Piece("P", number, position, isWhite) {}
+
+        std::vector<Position> validMoves(const board &chess_board);
+   
+};
